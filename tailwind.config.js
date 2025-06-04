3bfda2584c8650fc90bfc54af94db1af8056642c
Zobayer <mdzobayerhossen520@gmail.com>
2025-06-04 15:31:32 +0600

    NextApp theam setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: "class",
+  darkMode: "class", //class or media
   theme: {
     extend: {},
   },
