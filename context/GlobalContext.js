a54715d57ea146dc4dedcbed366ec02fc5ef14c6
Zobayer <mdzobayerhossen520@gmail.com>
2025-06-04 14:21:52 +0600

    Nextjs New App

---
+++ context/GlobalContext.js
@@ -0,0 +1,21 @@
+
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+const GlobalContext = createContext();
+export const GlobalProvider = ({ children }) => {
+  const [loding, setLoding] = useState(null);
+  const [conversation, setConversation] = useState([]);
+  // const []
+
+  return (
+    <GlobalContext.Provider
+      value={{ loding, setLoding, conversation, setConversation }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(GlobalContext);
